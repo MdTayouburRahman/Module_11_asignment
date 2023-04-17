e5b9cba20e0382180a57840d776198d03846d9b0
MdTayouburRahman <mobihat2014@gmail.com>
2023-04-17 17:59:23 +0600

    second commit

---
+++ onlycode.js
@@ -0,0 +1,13 @@
+function multiplicationTable(num) {
+    for (let i = 1; i <= 10; i++) {
+        const product = num * i;
+        console.log(`${num} x ${i} = ${product}`);
+    }
+}
+
+// Test the function with different input numbers
+multiplicationTable(5);
+console.log("----------");
+multiplicationTable(7);
+console.log("----------");
+multiplicationTable(12);
